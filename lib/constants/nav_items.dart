2815c413387c0753ea5647a722435647bfdde92f
Jahid <71091614+jahidkhandakar@users.noreply.github.com>
2025-12-03 15:41:16 +0600

    Add List of navIcons

--- lib/constants/nav_items.dart
+++ lib/constants/nav_items.dart
@@ -1,7 +1,17 @@
+import 'package:flutter/material.dart';
+
 List<String> navItems = [
   "Home",
   "Skills",
   "Projects",
   "Blog",
   "Contact",
+];
+
+List<IconData> navIcons = [
+  Icons.home,
+  Icons.android_outlined,
+  Icons.work,
+  Icons.web,
+  Icons.quick_contacts_mail,
 ];
